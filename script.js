e3d69f3b20e81623a3fed88bab2a73d18d1ebabc
unrelentin <augmentedvisionjcd@gmail.com>
2024-11-23 12:13:52 +0700

    added base grid

---
+++ script.js
@@ -0,0 +1,20 @@
+const squareGrid = document.querySelector(".container");
+
+function makeTheGrids (theGrids) {
+    for (i = 0; i < theGrids; i++) {
+        const gridColumns = document.createElement("div");
+        gridColumns.classList.add("gridColumns");
+        gridColumns.style.cssText = "width: 25px; height: 25px; background-color: white; border: 1px solid black";
+        squareGrid.appendChild(gridColumns);      //Don't use " " or it will think it's a string
+
+        for (j = 0; j < theGrids; j++) {             //use two different loops' variables "i" and "j" to prevent it from disrupting each other
+            const gridRows = document.createElement("div");
+            gridRows.classList.add("gridRows")
+            gridRows.style.cssText= "width: 25px; height: 25px; background-color: white; border: 1px solid black";
+            gridColumns.appendChild(gridRows);
+        }
+    }
+};
+
+makeTheGrids(16);
+//Create a function that will append the grids as "square divs" children of squareGrid

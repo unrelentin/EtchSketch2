6be431a08a35fe096cf214dd84d25959c1ce711c
unrelentin <augmentedvisionjcd@gmail.com>
2024-11-23 13:03:52 +0700

    added hovering to color function

--- script.js
+++ script.js
@@ -6,15 +6,26 @@ function makeTheGrids (theGrids) {
         gridColumns.classList.add("gridColumns");
         gridColumns.style.cssText = "width: 25px; height: 25px; background-color: white; border: 1px solid black";
         squareGrid.appendChild(gridColumns);      //Don't use " " or it will think it's a string
-
+        gridColumns.addEventListener("mouseover", () => {
+            gridColumns.style.backgroundColor = "black";         //Don't use style.cssText, it will overwrite the whole thing
+        });
+        
+        
         for (j = 0; j < theGrids; j++) {             //use two different loops' variables "i" and "j" to prevent it from disrupting each other
             const gridRows = document.createElement("div");
             gridRows.classList.add("gridRows")
             gridRows.style.cssText= "width: 25px; height: 25px; background-color: white; border: 1px solid black";
             gridColumns.appendChild(gridRows);
+            gridRows.addEventListener ("mouseover", () => {
+                gridRows.style.backgroundColor = "black";
+            });
         }
     }
 };
 
 makeTheGrids(16);
+
+
+
+
 //Create a function that will append the grids as "square divs" children of squareGrid
